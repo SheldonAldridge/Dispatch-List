2fb8ef928ecc72d6f5a31cf1068d392232488298
Sheldon <sheldonaldridge@yahoo.com>
2023-07-15 17:02:29 +0200

    Added CBAR cars and areas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,58 @@
-let districtBtn = document.querySelector(".basset");
+let bassetBtn = document.querySelector(".basset");
+let bassetCarList = document.querySelector(".basset-cars-list");
+let bassetCar = document.querySelector(".basset-cars"); // Image
+
+let districtBtn = document.querySelector(".district");
 let districtCarList = document.querySelector(".district-cars-list");
-let districtCar = document.querySelector(".district-cars");
-let bassetCar = document.querySelector(".basset-cars");
+let districtCar = document.querySelector(".district-cars"); // Image
+
+let cbarBtn = document.querySelector(".cbar");
+let cbarCarList = document.querySelector(".cbar-cars-list");
+let cbarCar = document.querySelector(".cbar-cars"); // Image
+
+bassetBtn.addEventListener('click', () => {
+    districtCarList.style.display = "none";
+    districtCar.style.display = "none";
+
+    bassetCarList.style.display = "block";
+    bassetCar.style.display = "block";
+
+    cbarCarList.style.display = "none";
+    cbarCar.style.display = "none";
+
+    cbarBtn.style.backgroundColor = "#F26835";
+    districtBtn.style.backgroundColor = "#F26835";
+    bassetBtn.style.backgroundColor = "#F26835";
+})
 
 districtBtn.addEventListener('click', () => {
-    districtCarList.style.display="None";
-    districtCar.style.display="None";
-    bassetCar.classList.remove("basset-cars");
+    districtCarList.style.display = "block";
+    districtCar.style.display = "block";
+
+    bassetCarList.style.display = "none";
+    bassetCar.style.display = "none";
+
+    cbarCarList.style.display = "none";
+    cbarCar.style.display = "none";
+
+    districtBtn.style.backgroundColor = "#3264A6";
+    bassetBtn.style.backgroundColor = "#3264A6";
+    cbarBtn.style.backgroundColor = "#3264A6";
 })
+
+cbarBtn.addEventListener('click', () => {
+    districtCarList.style.display = "none";
+    districtCar.style.display = "none";
+
+    bassetCarList.style.display = "none";
+    bassetCar.style.display = "none";
+
+    cbarCarList.style.display = "block";
+    cbarCar.style.display = "block";
+    cbarCar.style.marginTop = "10rem";
+
+    districtBtn.style.backgroundColor = "#DB2304";
+    bassetBtn.style.backgroundColor = "#DB2304";
+    cbarBtn.style.backgroundColor = "#DB2304";
+})
+
