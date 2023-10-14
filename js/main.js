bee54f3eb2fc012a817ce1ca06fa1c12167a6245
Sheldon <sheldonaldridge@yahoo.com>
2023-10-14 19:20:08 +0200

    add click event
    add click event to button. console.log button clicked. to indicate event listener is working

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//button click to toggle navbar to open/close
+
+const navButton = document.querySelector('.btn-dark');
+
+navButton.addEventListener('click', () =>{
+    console.log("button clicked");
+})
