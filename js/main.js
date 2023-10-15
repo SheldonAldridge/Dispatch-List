baefd14fd9a447c3248ce53a495a44f0686f13c9
Sheldon <sheldonaldridge@yahoo.com>
2023-10-15 18:14:38 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 //button click to toggle navbar to open/close
 
 const navButton = document.querySelector('.btn-dark');
+const navbar = document.querySelector('nav');
+const arrowOpen = document.querySelector('#arrow');
+let isOpen = false
 
 navButton.addEventListener('click', () =>{
-    console.log("button clicked");
+
+    isOpen = !isOpen
+    
+    if(isOpen){
+        navbar.classList.add('close-nav');
+        navbar.classList.remove('open-nav');
+        arrowOpen.classList.add('rotate-close');
+        arrowOpen.classList.remove('rotate-open');
+    }
+    else{
+        navbar.classList.add('open-nav');
+        navbar.classList.remove('close-nav');
+        arrowOpen.classList.add('rotate-open');
+        arrowOpen.classList.remove('rotate-close');
+    }
 })
